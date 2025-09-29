5c61282c5c1f55e5d57d2fe156a04fd17ae0582c
maehak19@naver.com <maehak19@naver.com>
2025-09-29 17:12:58 +0900

    v0.3 types/main.go

--- week06/types/main.go
+++ week06/types/main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"strings"
+	"reflect"
 )
 
 func main() {
-	fmt.Println(math.Round(2.21))
-	fmt.Println(math.Ceil(2.21))
-	fmt.Println(strings.Title("go developer!"))
-	fmt.Println("KIm\nInha\t\"\\")
-	fmt.Println('2', '가')
+	fmt.Println(reflect.TypeOf(2.31))
+	fmt.Println(reflect.TypeOf("Kim Inha"))
+	fmt.Println(reflect.TypeOf(true))
+	fmt.Println(reflect.TypeOf('A'))
+	fmt.Println(reflect.TypeOf(19))
 }
