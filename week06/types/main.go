0f50bd38eb62aab9c4f6e73c87b08b5532de8fe2
maehak19@naver.com <maehak19@naver.com>
2025-09-29 17:07:00 +0900

    v0.2 types/main.go

--- week06/types/main.go
+++ week06/types/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	"math"
+	"strings"
 )
 
 func main() {
 	fmt.Println(math.Round(2.21))
-	fmt.Println(math.ceil(2.21))
-	fmt.Println(strings.title("go developer!"))
+	fmt.Println(math.Ceil(2.21))
+	fmt.Println(strings.Title("go developer!"))
+	fmt.Println("KIm\nInha\t\"\\")
+	fmt.Println('2', '가')
 }
