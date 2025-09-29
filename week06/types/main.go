9b573edda40c45204f918cd44c13b6ae98501a14
maehak19@naver.com <maehak19@naver.com>
2025-09-29 17:39:49 +0900

    v0.4 types/main.go

--- week06/types/main.go
+++ week06/types/main.go
@@ -6,9 +6,27 @@ import (
 )
 
 func main() {
-	fmt.Println(reflect.TypeOf(2.31))
-	fmt.Println(reflect.TypeOf("Kim Inha"))
-	fmt.Println(reflect.TypeOf(true))
-	fmt.Println(reflect.TypeOf('A'))
-	fmt.Println(reflect.TypeOf(19))
+	//var id int16
+	//var name string
+	//var gpa float32
+
+	//id = 999
+	//name = "Kim Inha"
+	//gpa = 3.99
+
+	// var id int16 = 999
+	// var name string = "Kim Inha"
+	// var gpa float32 = 3.99
+
+	// var id = 999
+	// var name = "Kim Inha"
+	// var gpa = 3.99
+
+	id := 999
+	name := "Kim Inha"
+	gpa := 3.99
+	//단축기호를 통한
+
+	fmt.Println("학번은", id, reflect.TypeOf(id), ", 이름은", name, reflect.TypeOf(name))
+	fmt.Println("평점 :", gpa, reflect.TypeOf(gpa))
 }
